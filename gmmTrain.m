2d1cc1085124b59803fe9def4add9f8a4d457e12
Tommy Li <tommyli113@gmail.com>
2017-03-21 20:11:06 -0400

    WIP initialize theta for gmmtrain

--- gmmTrain.m
+++ gmmTrain.m
@@ -15,3 +15,34 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
 %                                          is a vector
 %                            gmm.cov     : DxDxM matrix of covariances. 
 %                                          (:,:,i) is for i^th mixture
+
+  gmms = {};
+
+  training_dir = strsplit(ls(training_dir));
+  % remove last element (always an empty string)
+  training_dir(length(training_dir)) = [];
+
+  for dir_index=1:length(training_dir)
+    new_gmm = struct();
+    new_gmm.name = training_dir{dir_index};
+
+    speaker_directory = dir([training_dir, filesep, training_dir{dir_index}, filesep, '*', 'mfcc']);
+
+    speaker_files = {};
+    for file_index=1:length(speaker_directory)
+      speaker_files = [speaker_files, textread([speaker_directory, filesep, speaker_directory(file_index).name], '%s', 'delimiter', '\n')];
+    end
+    
+    [mn, weight, covariance] = initialize_theta(speaker_files, M);
+
+
+
+
+    gmms = [gmms, new_gmm];
+  end
+return
+
+
+function [mn, weight, covariance] = initialize_theta( files, M )
+  weight = ones(1, M) * 1/M;
+return
