f8f155ed92f1dee0b5db2e3465e742152c9c8d53
Tommy Li <tommyli113@gmail.com>
2017-03-24 17:55:57 -0400

    finished theta init and fixed bugs

--- gmmTrain.m
+++ gmmTrain.m
@@ -18,7 +18,7 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
 
   gmms = {};
 
-  training_dir = strsplit(ls(training_dir));
+  training_dir = strsplit(ls(dir_train));
   % remove last element (always an empty string)
   training_dir(length(training_dir)) = [];
 
@@ -26,23 +26,34 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
     new_gmm = struct();
     new_gmm.name = training_dir{dir_index};
 
-    speaker_directory = dir([training_dir, filesep, training_dir{dir_index}, filesep, '*', 'mfcc']);
+    speaker_directory = dir([dir_train, filesep, training_dir{dir_index}, filesep, '*', '.mfcc']);
 
     speaker_files = {};
     for file_index=1:length(speaker_directory)
-      speaker_files = [speaker_files, textread([speaker_directory, filesep, speaker_directory(file_index).name], '%s', 'delimiter', '\n')];
+      new_file = textread([dir_train, filesep, training_dir{dir_index}, filesep, speaker_directory(file_index).name], '%s', 'delimiter', '\n');
+      speaker_files = [speaker_files; new_file];
     end
     
     [mn, weight, covariance] = initialize_theta(speaker_files, M);
 
+    % TODO: rest of stuff
 
 
 
-    gmms = [gmms, new_gmm];
+    gmms = [gmms; new_gmm];
   end
 return
 
 
 function [mn, weight, covariance] = initialize_theta( files, M )
+  % means are selected randomly
+  rand_index = floor(length(files) * rand(1));
+  mn = files(rand_index:(rand_index + M - 1), :)';
+
+  % weights are initially uniform
   weight = ones(1, M) * 1/M;
+
+  % covariances are identity matrices
+  covariance = repmat(eye(size(files, 2)), 1, 1, M);
+
 return
