02f36a05b4811f21078bde7f4df72567567279da
Tommy Li <tommyzli@gmail.com>
2017-03-29 21:41:34 -0400

    WIP

--- gmmTrain.m
+++ gmmTrain.m
@@ -11,12 +11,10 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
 %                        with this structure:
 %                            gmm.name    : string - the name of the speaker
 %                            gmm.weights : 1xM vector of GMM weights
-%                            gmm.means   : DxM matrix of means (each column 
+%                            gmm.means   : DxM matrix of means (each column
 %                                          is a vector
-%                            gmm.cov     : DxDxM matrix of covariances. 
+%                            gmm.cov     : DxDxM matrix of covariances.
 %                                          (:,:,i) is for i^th mixture
-
-  gmms = {};
 
   training_dir = strsplit(ls(dir_train));
   % remove last element (always an empty string)
@@ -37,7 +35,7 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
       new_file = dlmread(strcat(dir_train, filesep, training_dir{dir_index}, filesep, speaker_directory(file_index).name));
       mfcc_vectors = [mfcc_vectors; new_file];
     end
-    
+
     theta = initialize_theta(mfcc_vectors, M);
 
     improvement = Inf;
@@ -48,9 +46,9 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
 
       disp(sprintf('Old L: %s       New L: %s', prev_L, L));
 
-      improvement = abs(L - prev_L);
+      improvement = L - prev_L;
       prev_L = L;
-      %disp(sprintf('Improvement: %s', num2str(improvement)));
+      disp(sprintf('Improvement: %s', num2str(improvement)));
 
       i = i + 1;
     end
@@ -58,7 +56,8 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
     new_gmm.weights = theta.weight;
     new_gmm.means = theta.mean;
     new_gmm.cov = theta.covariance;
-    gmms = [gmms; new_gmm];
+    %gmms = [gmms; new_gmm];
+    gmms{dir_index} = new_gmm;
   end
   return
 end
@@ -66,9 +65,15 @@ end
 
 function theta = initialize_theta( mfcc, M )
   % means are selected randomly
-  %rand_index = floor(length(mfcc) * rand(1));
-  %mn = mfcc(rand_index:(rand_index + M - 1), :)';
-  mn = mfcc(1:M, :)';
+  %{
+  rand_indexes = randperm(size(mfcc, 1), M);
+  for i=1:M
+    mn(:, i) = mfcc(rand_indexes(i), :);
+  end
+  %}
+  for i=1:M
+    mn(:, i) = mfcc(1, :);
+  end
 
   % weights are initially uniform
   weight = ones(1, M) * 1/M;
@@ -90,18 +95,19 @@ function [log_likelihood, theta] = computeLikelihoodAndUpdateParameters( theta, 
   x = size(mfcc_vectors, 1);
   d = size(mfcc_vectors, 2);
 
-  cov = diag(theta.covariance(:, :, M));
-
   % log(b_m(x_t))
-  log_b_m_xt = -1 * sum(...
-    (mfcc_vectors - ((ones(x, 1) * theta.mean(:, M)') .^ 2) ./ (2 .* ((ones(x, 1) * cov') .^ 2))) ...
-    - (M/2 * log(2*pi)) ...
-    - (0.5 * log(prod(cov .^ 2))), 2);
+  for i=1:M
+    cov = diag(theta.covariance(:, :, i));
+    section_1 = -1 * sum( ...
+      ((mfcc_vectors - ((ones(x, 1) * theta.mean(:, i)'))) .^ 2) ./ (2 .* cov(:, :, 1))', 2);
+
+    section_2 = ((d/2) * log(2*pi)) + (0.5 * prod(cov));
+    log_b_m_xt(:, i) = section_1 - section_2;
+  end
 
   b_m_xt = exp(log_b_m_xt);
-  weighted_probs = [];
   for i=1:M
-    weighted_probs = [weighted_probs; theta.weight(i) * b_m_xt(i)];
+    weighted_probs(:, i) = theta.weight(i) .* b_m_xt(:, i);
   end
 
   p_theta_xt = sum(weighted_probs, 2);
@@ -109,25 +115,27 @@ function [log_likelihood, theta] = computeLikelihoodAndUpdateParameters( theta, 
   log_likelihood = sum(log(p_theta_xt));
 
   %  ----- update params
-  p_m_given_xt = [];  % P(m | x_t, theta)
+  % P(m | x_t, theta)
   for i=1:M
-    p_m_given_xt = [p_m_given_xt; weighted_probs(i) ./ p_theta_xt(i)];
+    p_m_given_xt(:, i) = weighted_probs(:, i) ./ p_theta_xt;
   end
 
+  theta.weight = sum(p_m_given_xt) / x;
+
   for j=1:M
-    sum_p_m_given_xt = sum(p_m_given_xt(j));
+    sum_p_m_given_xt = sum(p_m_given_xt(:, j));
 
-    % new weight
-    theta.weight(j) = sum_p_m_given_xt / x;
-
-    % new mean
     multiplier = ones(1, x) - 1;
     multiplier(1) = 1;
-    theta.mean(j) = sum((p_m_given_xt(j) * multiplier) * mfcc_vectors) / sum_p_m_given_xt;
+    theta.mean(:, j) = sum((p_m_given_xt(:, j) * multiplier) * mfcc_vectors) / sum_p_m_given_xt;
 
-    % new covariance
-    cov_vector = (sum((p_m_given_xt(j) * multiplier) * (mfcc_vectors .^ 2)) / sum_p_m_given_xt) - (theta.mean(j) ^ 2);
-    theta.covariance(:, :, j) = diag(cov_vector);
+    %{
+    mthCov = sum((p_m_given_xt(:, j) * multiplier)' * (mfcc_vectors .^ 2)) / sum_p_m_given_xt;
+    theta.covariance(:, :, j) = diag(mthCov(1, :)' - (theta.mean(:, j) .^ 2));
+    %}
+    cov_section_1 = sum((p_m_given_xt(:, j) * multiplier)' * (mfcc_vectors .^2));
+    theta.covariance(:, :, j) = diag((cov_section_1 / sum_p_m_given_xt) - (theta.mean(:, j) .^ 2)');
+
   end
 
 end
