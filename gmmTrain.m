296f635e645d065829b295a638e21f041419042a
Tommy Li <tommyli113@gmail.com>
2017-03-30 16:36:43 -0400

    make matrix dimensions work on cdf

--- gmmTrain.m
+++ gmmTrain.m
@@ -96,7 +96,7 @@ function [log_likelihood, theta] = computeLikelihoodAndUpdateParameters( theta, 
   for i=1:M
     cov = diag(theta.cov(:, :, i));
     section_1 = -1 * sum( ...
-      ((mfcc_vectors - ((ones(x, 1) * theta.means(:, i)'))) .^ 2) ./ (2 .* cov)', 2);
+      ((mfcc_vectors - ((ones(x, 1) * theta.means(:, i)'))) .^ 2) ./ (ones(x, 1) * (2 .* cov)'), 2);
 
     section_2 = ((d/2) * log(2*pi)) + (0.5 * prod(cov));
     log_b_m_xt(:, i) = section_1 - section_2;
