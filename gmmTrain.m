10beb92c5a7e1292be9b12143a1d63a2345beefb
Tommy Li <tommyzli@gmail.com>
2017-03-31 17:00:18 -0400

    shame on me

--- gmmTrain.m
+++ gmmTrain.m
@@ -97,7 +97,7 @@ function [log_likelihood, theta] = computeLikelihoodAndUpdateParameters( theta, 
     section_1 = -1 * sum( ...
       ((mfcc_vectors - ((ones(x, 1) * theta.means(:, i)'))) .^ 2) ./ (ones(x, 1) * (2 .* cov)'), 2);
 
-    section_2 = ((d/2) * log(2*pi)) + (0.5 * prod(log(cov)));
+    section_2 = ((d/2) * log(2*pi)) + (0.5 * log(prod(cov)));
     log_b_m_xt(:, i) = section_1 - section_2;
     b_m_xt(:, i) = exp(log_b_m_xt(:, i));
     weighted_probs(:, i) = theta.weights(i) * b_m_xt(:, i);
